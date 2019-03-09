d89e0426db18c6493549d728c212ef4b36ecadf2
Keita Ohshiro <keita.ohshiro@gmail.com>
2019-03-09 11:03:49 -0500

    New updates for Heroku deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wpm86n)5g1)f)d9dtbed=tt^3fk=2*%sj^5ddt(%4)1108es4q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
